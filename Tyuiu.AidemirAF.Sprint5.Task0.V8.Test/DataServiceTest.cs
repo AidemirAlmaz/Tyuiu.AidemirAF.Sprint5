b04023a0d84df30832af13a8a7a7a3138c727088
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-17 17:45:46 +0500

    Task0

--- Tyuiu.AidemirAF.Sprint5.Task0.V8.Test/DataServiceTest.cs
+++ Tyuiu.AidemirAF.Sprint5.Task0.V8.Test/DataServiceTest.cs
@@ -9,12 +9,10 @@ namespace Tyuiu.AidemirAF.Sprint5.Task0.V8.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string directory = @"C:\Users\Honor\source\repos\Tyuiu.AidemirAF.Sprint5\Tyuiu.AidemirAF.Sprint5.Task0.V8\bin\Debug";
-            string file = "OutPutFileTask0.txt";
-            string path = Path.Combine(directory, file);
+            string path = @"C:\Users\Honor\source\repos\Tyuiu.AidemirAF.Sprint5\Tyuiu.AidemirAF.Sprint5.Task0.V8\bin\Debug\net8.0\OutPutFileTask0.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
-            bool wait = false;
+            bool wait = true;
             Assert.AreEqual(wait, fileExists);
         }
     }

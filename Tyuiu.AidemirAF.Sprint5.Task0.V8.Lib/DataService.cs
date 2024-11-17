43596e422270d2d5d93cc6f2dbdca644e40f186d
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-17 17:58:52 +0500

    Task0

--- Tyuiu.AidemirAF.Sprint5.Task0.V8.Lib/DataService.cs
+++ Tyuiu.AidemirAF.Sprint5.Task0.V8.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.AidemirAF.Sprint5.Task0.V8.Lib
         public string SaveToFileTextData(int x)
         {
             string path = $@"{Path.GetTempPath()}OutPutFileTask0.txt";
-            double y = Math.Round((Math.Pow(x, 3) - 1) / 4 * Math.Pow(x, 2), 3);
+            double y = Math.Round((Math.Pow(x, 3) - 1) / (4 * Math.Pow(x, 2)), 3);
             File.WriteAllText(path, Convert.ToString(y));
             return path;
         }

daabd307acb5b2ac7b848f891f7f4f31c026b476
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-17 21:58:58 +0500

    Task1

---
+++ Tyuiu.AidemirAF.Sprint5.Task1.V12/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.AidemirAF.Sprint5.Task1.V12.Lib;
+
+namespace Tyuiu.AidemirAF.Sprint5.Task1.V12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue = -5;
+            int stopValue = 5;
+
+            DataService ds = new DataService();
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                  *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            Console.WriteLine("startValue = " + startValue);
+            Console.WriteLine("stopValue = " + stopValue);
+
+            Console.WriteLine("*****************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                        *");
+            Console.WriteLine("*****************************************************************************************************");
+
+            string res = ds.SaveToFileTextData(startValue, stopValue);
+            Console.WriteLine("Файл:" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}

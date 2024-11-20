df5f2f01e42083ecbe95d383a9b41cea76273415
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-20 13:55:34 +0500

    Task2

---
+++ Tyuiu.AidemirAF.Sprint5.Task2.V21.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.AidemirAF.Sprint5.Task2.V21.Lib;
+
+namespace Tyuiu.AidemirAF.Sprint5.Task2.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.GetTempFileName();
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}

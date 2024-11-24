dcff4c0e8875badd636c24b2c76eaf5a56170ce1
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-24 17:26:22 +0500

    Task3

---
+++ Tyuiu.AidemirAF.Sprint5.Task3.V24.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.AidemirAF.Sprint5.Task3.V24.Lib
+{
+    public class DataService : ISprint5Task3V21
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Path.GetTempPath()}OutPutFileTask3.bin";
+            double y = Math.Round(6.1 * Math.Pow(x, 2) + 0.23 * Math.Pow(x, 2) + 1.04 * x, 3);
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+            return path;
+        }
+    }
+}

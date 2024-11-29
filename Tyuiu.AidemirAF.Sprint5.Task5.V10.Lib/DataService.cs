4593dc7a4b547136c261a3438d94a13da1383d39
Tyuiu.AidemirAF@outlook.com <almazaydemir@yandex.ru>
2024-11-30 00:57:34 +0500

    Task5

--- Tyuiu.AidemirAF.Sprint5.Task5.V10.Lib/DataService.cs
+++ Tyuiu.AidemirAF.Sprint5.Task5.V10.Lib/DataService.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Globalization;
+using System.Runtime.CompilerServices;
 using tyuiu.cources.programming.interfaces.Sprint5;
     
 namespace Tyuiu.AidemirAF.Sprint5.Task5.V10.Lib
@@ -10,28 +11,27 @@ namespace Tyuiu.AidemirAF.Sprint5.Task5.V10.Lib
             double res = 0;
             using (StreamReader reader = new StreamReader(path))
             {
-                string line;
-                string inputString = "14 18 15 3.76 8.53 9.56 -4.52 -10 15.29 17 7 7 7 -7.09 5.29 11.48 -1 -3.81 17.08 -7.56"; 
-                double sum = 0;
-                string[] numberStrings = inputString.Split(new char[] { ' ', ',', '\t' }, StringSplitOptions.RemoveEmptyEntries);
-
-                foreach (string numberString in numberStrings)
                 {
-                    if (double.TryParse(numberString, out double number))
+                    string[] lines = File.ReadAllLines(path);
+
+                    foreach (string line in lines)
                     {
-                        if (number % 2 == 0)
+                        string[] numberStrings = line.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries); // Разделение строки на числа
+
+                        foreach (string numberString in numberStrings)
                         {
-                            sum += number;
+                            if (double.TryParse(numberString, NumberStyles.Any, CultureInfo.InvariantCulture, out double number))
+                            {
+                                if (number % 2 == 0)
+                                {
+                                    res += number;
+                                }
+                            }
                         }
                     }
-                    else
-                    {
-                        // Обработка случая, если строка не может быть преобразована в число (можно пропустить или вывести сообщение об ошибке)
-                        //Console.WriteLine($"Не удалось преобразовать '{numberString}' в число.");
-                    }
                 }
-                return sum;
             }
+            return res;
         }
     }
 }
